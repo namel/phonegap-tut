0658029530d26688c2e808f1c3ab7e469d075225
Gary Weiss <garyweiss74@gmail.com>
2013-06-12 16:07:38 +0300

    use a home view classw

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
 var app = {
 
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-
-    findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
 
     showAlert: function (message, title) {
         if (navigator.notification) {
@@ -22,10 +11,8 @@ var app = {
 
     initialize: function() {
         var self = this;
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         this.store = new MemoryStore(function() {
-            self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
     }
 
